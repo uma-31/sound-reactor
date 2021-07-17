1252d6b93807f610f09cb4109b8418dd9fa7ed0f
uMa <ysmst35@gmail.com>
2021-07-17 12:54:53 +0900

    feat: addコマンドを実装

--- package/bot.py
+++ package/bot.py
@@ -1,9 +1,7 @@
-from argparse import ArgumentParser
 from discord import Embed
-from discord.errors import InvalidArgument
 from package.exceptions import InvalidFileType
 from discord.ext import commands
-from .data_manage import regist_sound, remove_sound
+from .data_manage import is_sound_exists, regist_sound
 
 
 class SoundReactor(commands.Bot):
@@ -19,4 +17,67 @@ class SoundReactor(commands.Bot):
         config : dict[str, Any]
             各種設定
         """
-        pass
+        super().__init__(
+            command_prefix='.' if 'prefix' not in config else config['prefix'])
+
+        @self.command()
+        async def add(ctx: commands.Context,
+                      alias: str) -> None:
+            await self.__on_add_command(ctx, alias)
+
+    async def __reply_error_message(self,
+                                    ctx: commands.Context,
+                                    message: str):
+        """
+        エラーメッセージを返信する
+
+        ...
+
+        Parameters
+        ----------
+        ctx : Context
+            https://discordpy.readthedocs.io/ja/latest/ext/commands/api.html#discord.ext.commands.Context
+
+        message : str
+            送信するメッセージ
+        """
+        embed = Embed(title='Error!',
+                      description=message,
+                      color=0xfffc66)
+
+        await ctx.send(embed=embed)
+
+    async def __on_add_command(self,
+                               ctx: commands.Context,
+                               alias: str) -> None:
+        """
+        Parameters
+        ----------
+        ctx : Context
+            https://discordpy.readthedocs.io/ja/latest/ext/commands/api.html#discord.ext.commands.Context
+
+        *args : list[str]
+            引数のリスト
+        """
+        if ctx.message is None:
+            await self.__reply_error_message(ctx, '予期せぬエラーが発生しました。')
+            return
+
+        n_attachments = len(ctx.message.attachments)
+
+        if n_attachments == 0:
+            await self.__reply_error_message(ctx, 'ファイルが添付されていません！')
+            return
+        elif n_attachments > 1:
+            await self.__reply_error_message(ctx, 'addコマンドで添付できるファイルは1つです。')
+            return
+
+        if is_sound_exists(alias):
+            await self.__reply_error_message(ctx, f'{alias}は既に登録されています。')
+            return
+
+        try:
+            await regist_sound(alias, ctx.message.attachments[0])
+        except InvalidFileType as e:
+            await self.__reply_error_message(ctx, e.args[0])
+            return
